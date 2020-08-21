1168af483a925944d4b4477879f069f6e33a3f83
Simon Jung <simon.hb.jung@hotmail.com>
2020-08-21 00:33:37 +0000

    user can sign in/ login

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,7 @@
 class AboutController < ApplicationController
+  #before_filter :authorize
+  #if you want to authorize user before accessing a page, use above code but we do not want to do that for any pages
+
   def show
     #in views, finds directory about, then renders show
   end
