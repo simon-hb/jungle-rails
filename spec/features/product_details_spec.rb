2cebc22311ba1b06a1a98502f646fe80c0796848
Simon Jung <simon.hb.jung@hotmail.com>
2020-08-25 23:55:20 +0000

    add feature spec test for product detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,14 +16,18 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  #SCENARIO
+  scenario "Users can navigate from the home page to the product detail page by clicking on a product" do
     # ACT
     visit root_path
 
-    # DEBUG
-    # save_screenshot
+    #DEBUG
+    find(".product", match: :first).click_link('Details')
+    save_screenshot
+    save_screenshot('product_page.png')
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css('.product-detail')
+
   end
 end
